a940108ae916cac4a91e15d685fc0cd0312fbbec
sidasari <112645018+sidasari@users.noreply.github.com>
2023-02-16 16:04:15 +0300

    Update main.tf

--- DevOps_Project_2/Terraform/main.tf
+++ DevOps_Project_2/Terraform/main.tf
@@ -1,21 +1,8 @@
-data "aws_ami" "amazon-linux" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm-*-x86_64-gp2"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
 
 resource "aws_instance" "dev_machine" {
   ami = "ami-0dfcb1ef8550277af"
   instance_type = "t2.micro"
-  key_name = "euran-jenkins"
+  key_name = "keypair"
 
   tags = {
     Environment = "dev"
