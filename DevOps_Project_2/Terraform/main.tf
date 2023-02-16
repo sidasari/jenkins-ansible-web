5e424476b1f2d5287e3062eaacfd2598de3b9fbb
sidasari <112645018+sidasari@users.noreply.github.com>
2023-02-16 16:07:49 +0300

    Update main.tf

--- DevOps_Project_2/Terraform/main.tf
+++ DevOps_Project_2/Terraform/main.tf
@@ -3,7 +3,6 @@ resource "aws_instance" "dev_machine" {
   ami = "ami-0dfcb1ef8550277af"
   instance_type = "t2.micro"
   key_name = "keypair"
-
   tags = {
     Environment = "dev"
     Name = "${var.name}-server"
